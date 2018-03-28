d3c141cbd808093bcd36ef1ed164721ae6006b22
ayakael <antoine.martin@protonmail.com>
2018-03-28 19:26:38 -0400

    [build.sh] Updated build script to remove debug eval command. [build.sh] Build script now has 'env' file at head and 'parser' file tail [cmd_deploy] Now uses the _OPT_DB_FILE variable instead of DB_FILE [cmd_deploy] now generates imageidList using DB_FILE if imageid not provided by user [cmd_exclude] Rewrote to operate as front end to db_set's exclude function [cmd_include] Added as front end to db_set's include function [cmd_update] Fixed some typos [db_set] Added as backend to DB_FILE trackid selection [env] Now defines _OPT_DB_FILE as .dam.db [parser] Updates parser to support cmd_deploy, cmd_include, cmd_exclude and cmd_update [print_meta_mtag] Last line now does not trail with ";" character

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ EXEC=dam
 
 echo -e "#!/bin/bash\n" > ${EXEC}
 
-for file in $(find src/ -type f); do
+for file in src/env $(find src/ -type f -not -name env -not -name parser) src/parser; do
     awk '!/^ *#/ && NF' ${file} >> ${EXEC}
 done
-echo 'eval $@' >> ${EXEC}
+
+
