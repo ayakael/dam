abef0cd0d3f9fcbc3964b0497d139bc3919305d1
ayakael <antoine.martin@protonmail.com>
2018-04-07 19:58:45 -0400

    [build.sh] Now adds bunc functions

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ EXEC=dam
 
 echo -e "#!/bin/bash\n" > ${EXEC}
 
-for file in src/env $(find src/ -type f -not -name env -not -name parser) src/parser; do
+for file in src/env $(find bunc/src -type f) $(find src/ -type f -not -name env -not -name parser) src/parser; do
     awk '!/^ *#/ && NF' ${file} >> ${EXEC}
 done
 
-
+chmod +x ${EXEC}
